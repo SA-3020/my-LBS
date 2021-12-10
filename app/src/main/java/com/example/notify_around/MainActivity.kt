ad68607460de0fe88b251b7113c586faf4e13d4c
mujahidnafees <rollno414@gmail.com>
2021-12-11 00:07:46 +0500

    get current user location

--- app/src/main/java/com/example/notify_around/MainActivity.kt
+++ app/src/main/java/com/example/notify_around/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.notify_around
 
+import android.annotation.SuppressLint
+import com.google.firebase.firestore.GeoPoint
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.notify_around.businessUser.activities.BUserDashboard
 import com.example.notify_around.databinding.ActivityMainBinding
@@ -14,6 +18,12 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.github.florent37.runtimepermission.kotlin.askPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -22,6 +32,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var fusedLocationProviderClient: FusedLocationProviderClient?=null
+    //to retrieve the device's last known location. The fused location provider is one of the location APIs in Google Play services
     private lateinit var auth: FirebaseAuth
     private lateinit var callbackManager: CallbackManager
     private lateinit var buttonFacebookLogin: LoginButton
@@ -30,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
 
+        getCurrentLocation()
         //redirect()
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
@@ -138,6 +151,66 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this,exception.message,Toast.LENGTH_LONG).show()
                 }
 
+        }
+    }
+
+
+    private fun askPermissionLocation(){
+        askPermission(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) {
+            getCurrentLocation()
+        }.onDeclined { e ->
+            if (e.hasDenied()) {
+                e.denied.forEach{
+                }
+                AlertDialog.Builder(this)
+                    .setMessage("Please enable Location")
+                    .setPositiveButton("Yes") { _, _ ->
+                        e.askAgain()
+                    }
+                    .setNegativeButton("no") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+            if (e.hasForeverDenied()) {
+                e.foreverDenied.forEach {
+
+                }
+                e.goToSettings();
+            } }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        try{
+            @SuppressLint("Missing Permission")
+            val location = fusedLocationProviderClient!!.getLastLocation()
+            location.addOnCompleteListener(object: OnCompleteListener<Location> {
+                override fun onComplete(loc: Task<Location>) {
+                    if(loc.isSuccessful){
+                        val currentLocation= loc.result as Location?
+                        if(currentLocation != null){
+                            val geoPoints=GeoPoint(currentLocation.latitude,currentLocation.longitude)
+                            UserManager.userLocation= geoPoints
+
+                        }
+                        else{
+                            askPermissionLocation()
+                        }
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity,"Current Location not found.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+        }
+        catch(se: Exception){
+            Log.e("TAG","Security Exception")
         }
     }
 }
--- app/src/main/java/com/example/notify_around/UserManager.kt
+++ app/src/main/java/com/example/notify_around/UserManager.kt
@@ -1,8 +1,10 @@
 package com.example.notify_around
 
 import com.example.notify_around.models.GeneralUser
+import com.google.firebase.firestore.GeoPoint
 
 object UserManager {
 
     var user:GeneralUser?=null
+    var userLocation:GeoPoint?=null
 }

764544c7ba2ea938e9018129c5d1b734e622bacb
mujahidnafees <rollno414@gmail.com>
2021-12-12 20:14:04 +0500

    Skills and Problems AI part.

--- app/src/main/java/com/example/notify_around/models/ProblemModel.kt
+++ app/src/main/java/com/example/notify_around/models/ProblemModel.kt
@@ -2,6 +2,7 @@ package com.example.notify_around.models
 
 import android.icu.text.SimpleDateFormat
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
 import java.sql.Time
 import java.util.*
 
@@ -10,6 +11,7 @@ class ProblemModel(
     var title: String = "",
     var description: String = "",
     var levelOfEmergency: String = "",
+    var geoPoints: GeoPoint?=null,
     var postedOn: Timestamp? = null,// = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date()),
     var postedBy: String = "",
     var dateAt: String = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date()),
--- app/src/main/java/com/example/notify_around/models/SkillModel.kt
+++ app/src/main/java/com/example/notify_around/models/SkillModel.kt
@@ -2,6 +2,7 @@ package com.example.notify_around.models
 
 import android.icu.text.SimpleDateFormat
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
 import java.sql.Time
 import java.util.*
 
@@ -9,6 +10,7 @@ class SkillModel(
     var id: String = "",
     var title: String = "",
     var description: String = "",
+    var geoPoints: GeoPoint?=null,
     var interests: MutableList<String> = arrayListOf(),
     var postedOn: Timestamp = Timestamp.now(),//SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date()),
     var postedBy: String = "",
